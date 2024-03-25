bd1c763c2b4d8ea80c57c19575a0d5f3f56f63be
Mark Fisher <mark.j.fisher@gmail.com>
2024-03-25 11:29:46 +0000

    Make each word have a separate delay from 0-50ms

--- src/main/kotlin/http/simple/QuotesController.kt
+++ src/main/kotlin/http/simple/QuotesController.kt
@@ -1,14 +1,18 @@
 package http.simple
 
 import data.Quotation
-import io.micronaut.core.async.annotation.SingleResult
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.scheduling.TaskExecutors
 import io.micronaut.scheduling.annotation.ExecuteOn
+import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
+import io.reactivex.FlowableEmitter
+import io.reactivex.FlowableOnSubscribe
+import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
+import kotlin.random.Random
 import quotable.QuotableClient
 
 @ExecuteOn(TaskExecutors.BLOCKING)
@@ -19,18 +23,21 @@ class QuotesController(
     private val bufferedQuotes: MutableList<Quotation> = mutableListOf()
 
     @Get("/random", produces = [MediaType.TEXT_PLAIN])
-    // @SingleResult
     fun fetchRandomQuotes(): Flowable<String> {
         if (bufferedQuotes.isEmpty()) {
-            // get some more quotes into the buffer
             bufferedQuotes.addAll(quotableClient.fetchRandomQuotes())
         }
         val quote = bufferedQuotes.removeFirst()
         val sentence = String.format("%s (%s)", quote.content, quote.author).trim()
+        println("Sending '${sentence}")
         val asWords = sentence.split(" ")
         val wordsWithSpaces = asWords.mapIndexed { index, word -> if (index != asWords.size - 1) "$word " else word }
 
         return Flowable.fromIterable(wordsWithSpaces)
-            .zipWith(Flowable.interval(200, TimeUnit.MILLISECONDS)) { word, _ -> word }
+            .concatMap { word ->
+                val delay = Random.nextFloat() * 50L // 0-50ms delay between words
+                Flowable.just(word).delay(delay.toLong(), TimeUnit.MILLISECONDS)
+            }
     }
+
 }
